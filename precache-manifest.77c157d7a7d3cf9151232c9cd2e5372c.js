131d53147bd4550ba9c5cf39527a295704ef6290
rkgallaway <rgallaway@paciolan.com>
2020-05-12 15:01:34 -0700

    Updates

---
+++ precache-manifest.77c157d7a7d3cf9151232c9cd2e5372c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd10318a519383f170e95801a37c055f",
+    "url": "/tetris-hooks/index.html"
+  },
+  {
+    "revision": "298e51629bf2af94a019",
+    "url": "/tetris-hooks/static/css/main.e50336d1.chunk.css"
+  },
+  {
+    "revision": "d65617f6a53a0dc55c1d",
+    "url": "/tetris-hooks/static/js/2.42418041.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris-hooks/static/js/2.42418041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298e51629bf2af94a019",
+    "url": "/tetris-hooks/static/js/main.44ab2f13.chunk.js"
+  },
+  {
+    "revision": "69eec482fa1f77167df2",
+    "url": "/tetris-hooks/static/js/runtime-main.81f12417.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris-hooks/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris-hooks/static/media/bg.2810fe6b.png"
+  }
+]);
